baa91050cb851f13c0fc34702b5c7ca4eddf6dbf
Yujin Chung <yjchung@promedius.ai>
2021-03-23 20:55:54 +0900

    get wave file info

---
+++ get_sound_info.py
@@ -0,0 +1,21 @@
+import collections
+import contextlib
+import sys
+import wave
+
+
+def read_wave(path):
+    """
+
+    :param path: .wav file
+    :return: pcm audio data, sample rate
+    """
+    with contextlib.closing(wave.open(path, 'rb')) as wf:
+        num_channels = wf.getnchannels()
+        assert num_channels == 1
+        sample_width = wf.getsampwidth()
+        assert sample_width == 2
+        sample_rate = wf.getframerate()
+        assert sample_rate in (8000, 16000, 32000, 48000)
+        pcm_data = wf.readframes(wf.getnframes())
+        return pcm_data, sample_rate
