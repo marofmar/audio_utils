1857ef6efb31cd57261d9cf695509450dc657b08
Yujin Chung <yjchung@promedius.ai>
2021-03-24 10:35:16 +0900

    tokenizer on Korean sentence

--- use_bert_tokenizer.py
+++ use_bert_tokenizer.py
@@ -8,6 +8,12 @@ def show_tokens(tokenizer, sentence):
     return tokenizer(sentence)['input_ids']
 
 
-show_tokens(tokenizer, "Honne")
+# show_tokens(tokenizer, "Honne")
 show_tokens(tokenizer, "create memories. Not jpegs.")
 show_tokens(tokenizer, "Just smile more.")
+show_tokens(tokenizer, "안녕하세요")
+# show_tokens(tokenizer, "sunrise in Georgia")
+show_tokens(tokenizer, "Hola, que tal?")
+show_tokens(tokenizer, "내 이름은 후추, 난 고양이야.")
+
+# how does this work on foreign languages other than English?
